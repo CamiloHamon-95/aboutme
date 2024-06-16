d53b2dba722906a38edff1e666fad954a894aac1
Camilo Rodriguez Hamon <camilohamon@gmail.com>
2024-06-15 23:06:40 -0500

    Section Presetation: Try a typing animation

--- src/Sections/IntroSection/index.js
+++ src/Sections/IntroSection/index.js
@@ -5,9 +5,16 @@ import './IntroSection.css';
 
 function IntroSection () {
     return (
-      <div>
-        <h1>Hi! I'm <span>Camilo</span>,</h1>
-        <h1>I'm a backend developer.</h1>
+      <div className='ct-intro'>
+        <div className='ct-description'>
+          <h1>Hi! I'm <span>Camilo</span>,</h1>
+          <h1>I'm a backend developer.</h1>
+          <h1 className='animated-text'>Working to be <span></span></h1>
+        </div>
+        <div className='ct-pizza'>
+          <h1>Here goes the pizza.</h1>
+        </div>
+        
       </div>
     )
   };
