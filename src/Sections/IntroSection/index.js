6833a609cbaad99e00cbe56853cb24f3b1b01d05
Camilo Rodriguez Hamon <camilohamon@gmail.com>
2024-06-16 11:42:37 -0500

    section presentation: updating font-family to p element

--- src/Sections/IntroSection/index.js
+++ src/Sections/IntroSection/index.js
@@ -9,7 +9,13 @@ function IntroSection () {
         <div className='ct-description'>
           <h1>Hi! I'm <span>Camilo</span>,</h1>
           <h1>I'm a backend developer.</h1>
-          <h1 className='animated-text'>Working to be <span></span></h1>
+          <p>
+            I find immense joy in building robust and scalable 
+            solutions with python's powerful frameworks.
+          </p>
+          <p>Every line of code i write fuels
+            my passion for crafting the backbone of innovative applications.
+          </p>
         </div>
         <div className='ct-pizza'>
           <h1>Here goes the pizza.</h1>
