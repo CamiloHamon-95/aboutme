9e73839303ee63c3048d40b2def2cc14dec48807
Camilo Rodriguez Hamon <camilohamon@gmail.com>
2024-06-17 10:54:15 -0500

    main: a bit of modifications

--- src/Sections/AboutSection/index.js
+++ src/Sections/AboutSection/index.js
@@ -14,6 +14,20 @@ function AboutSection () {
             </span>
           </h1>
         </div>
+        <div className='ct-description'>
+          <h1>Hi! I'm <span>Camilo</span>,</h1>
+          <h1>I'm a backend developer.</h1>
+          <p>
+            I find immense joy in building robust and scalable 
+            solutions with python's powerful frameworks.
+          </p>
+          <p>Every line of code i write fuels
+            my passion for crafting the backbone of innovative applications.
+          </p>
+        </div>
+        <div className='ct-pizza'>
+          <h1>Here goes the pizza.</h1>
+        </div>
       </section>
     )
   };
