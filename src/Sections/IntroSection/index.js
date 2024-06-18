1b5d39c1c6648d7a3840ff446d8c9bc26571193e
Camilo Rodriguez Hamon <camilohamon@gmail.com>
2024-06-18 16:53:50 -0500

    section-presentation: Adding photo

--- src/Sections/IntroSection/index.js
+++ src/Sections/IntroSection/index.js
@@ -1,3 +1,5 @@
+import profilePhoto from '../../staticImage/main_profile.png';
+
 import React from 'react';
 
 import './IntroSection.css';
@@ -8,7 +10,7 @@ function IntroSection () {
       <section className='ct-intro' id='presentation'>
         <div className='ct-description'>
           <h1>Hi! I'm <span>Camilo</span>,</h1>
-          <h1>I'm a backend developer.</h1>
+          <h2>I'm a backend developer.</h2>
           <p>
             I find immense joy in building robust and scalable 
             solutions with python's powerful frameworks.
@@ -17,8 +19,10 @@ function IntroSection () {
             my passion for crafting the backbone of innovative applications.
           </p>
         </div>
-        <div className='ct-pizza'>
-          <h1>Here goes the pizza.</h1>
+        <div className='ct-photo'>
+          <div className='photo-frame'>
+            <img src={profilePhoto} alt="Logo" />
+          </div>
         </div>
       </section>
     )
