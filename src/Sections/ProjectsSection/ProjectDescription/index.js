eddab7d31811648f825db4525a273b591139a107
Camilo Rodriguez Hamon <camilohamon@gmail.com>
2024-06-20 12:01:51 -0500

    main: section projects finished

--- src/Sections/ProjectsSection/ProjectDescription/index.js
+++ src/Sections/ProjectsSection/ProjectDescription/index.js
@@ -3,25 +3,55 @@ import './ProjectDescription.css';
 import React from 'react';
 
 function ProjectDescription ({title, content}) {
-    return (
-      <div className='ct-project-description'>
-        <h2>{title}</h2>
-        <div className='project-text-and-link'>
-          <p>{content}</p>
-          <div>
-            <a href='https://www.google.com/'>
-              <span>Texto de link</span>
-              <TodoIcon
-                type='youtube'
-              />
-              <TodoIcon
-                type='github'
-              />
-            </a>
+
+  const clickProject = (nameProject) => {
+    const actions = {
+        github: () => {
+          window.open('https://github.com/CamiloHamon-95/running-data-analyst','_blank');
+        },
+        linkedin: () => {
+          window.open('https://linkedin.com/in/camilohamon95/', '_blank');
+        },
+        youtube: () => {
+            window.open('https://www.youtube.com/@CamiloHamon', '_blank');
+        },
+        cv: () => {
+            window.open('https://drive.google.com/file/d/1PJoLgPK1nPgGZiecsXPufKBPLolpgXze/view?usp=sharing', '_blank');
+        }
+    };
+
+    if (actions[nameProject]) {
+        actions[nameProject]();
+    }
+  };
+
+  return (
+    <div className='ct-project-description'>
+      <h2>{title}</h2>
+      <div className='project-text-and-link'>
+        <p>{content}</p>
+        <div className='ct-project-links'>
+          <a href='https://www.google.com/'>
+            <span>Visit the project</span>
+          </a>
+          <div className='svg-icon-project'
+            onClick={() => window.open('https://github.com/CamiloHamon-95/running-data-analyst','_blank')}
+          >
+            <TodoIcon
+              type='youtube'
+            />
+          </div>
+          <div className='svg-icon-project'
+            onClick={() => window.open('https://github.com/CamiloHamon-95/running-data-analyst','_blank')}
+          >
+            <TodoIcon
+              type='github'
+            />
           </div>
         </div>
       </div>
-    )
-  };
+    </div>
+  )
+};
   
-  export { ProjectDescription };
+export { ProjectDescription };
--- src/Sections/ProjectsSection/index.js
+++ src/Sections/ProjectsSection/index.js
@@ -4,50 +4,60 @@ import './ProjectsSection.css';
 import React from 'react';
 
 function ProjectsSection () {
-    return (
-      <section  id='projects'>
-        <div className="general-title">
-          <h1>
-            Projects
-            <span>
-                Showcase
-            </span>
-          </h1>
-        </div>
-        <div className='ct-project'>
-          <ProjectBox
-            nameImage='image01'
-            title='Ecommerce Project'
-            content='Lorem Ipsu'
-            box={false}
-          />
-          <ProjectBox
-            nameImage='image01'
-            title='Todo APP with Calendar Integration'
-            content='Lorem Ipsu'
-            box={true}
-          />
-          <ProjectBox
-            nameImage='image01'
-            title='Trasnport IT'
-            content='Lorem Ipsu'
-            box={false}
-          />
-          <ProjectBox
-            nameImage='image01'
-            title='Telemarketing web app video'
-            content='Lorem Ipsu'
-            box={true}
-          />
-          <ProjectBox
-            nameImage='image01'
-            title='Tools from Data Analytics'
-            content='Lorem Ipsu'
-            box={false}
-          />
-        </div>
-      </section>
-    )
-  };
+
+  const texto=`
+  I'm Camilo, a Web Developer with 2 years of experience, 
+  passionate about technology and continuous learning. 
+  I find immense joy in building robust and scalable 
+  solutions with python's powerful frameworks. 
+  Every line of code i write fuels my passion for 
+  crafting the backbone of innovative applications.
+  `;
+
+  return (
+    <section  id='projects'>
+      <div className="general-title">
+        <h1>
+          Projects
+          <span>
+              Showcase
+          </span>
+        </h1>
+      </div>
+      <div className='ct-project'>
+        <ProjectBox
+          nameImage='image01'
+          title='Ecommerce Project'
+          content={texto}
+          box={false}
+        />
+        <ProjectBox
+          nameImage='image01'
+          title='Todo APP with Calendar Integration'
+          content={texto}
+          box={true}
+        />
+        <ProjectBox
+          nameImage='image01'
+          title='Trasnport IT'
+          content={texto}
+          box={false}
+        />
+        <ProjectBox
+          nameImage='image01'
+          title='Telemarketing web app video'
+          content={texto}
+          box={true}
+        />
+        <ProjectBox
+          nameImage='image01'
+          title='Tools from Data Analytics'
+          content={texto}
+          box={false}
+        />
+      </div>
+    </section>
+  )
+};
   
-  export { ProjectsSection };
+export { ProjectsSection };
